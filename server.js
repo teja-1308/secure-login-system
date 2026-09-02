7e11d2e9da6d7e8ba4da7ea553e4cff6f5029d53
D Teja <tejad1553@gmail.com>
2026-09-02 20:32:51 +0530

    fix: render deployment ready

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.post('/api/login', (req, res) => {
   if(found) res.json({msg: "Login Success - Welcome Teja!"});
   else res.json({msg: "Invalid Credentials"});
 });
-
-app.listen(3000, () => console.log("Running on 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
